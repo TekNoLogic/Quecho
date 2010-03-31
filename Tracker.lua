2e031700f5e9ff003e7734a69a46d4f15e297f06
tekkub <tekkub@gmail.com>
2010-03-30 22:00:27 -0600

    Ah undocumented changes to the API!

--- Tracker.lua
+++ Tracker.lua
@@ -13,18 +13,17 @@ end})
 
 
 local current = 0
-local function AddLine(text, maxwidth, heightUsed, r, g, b)
+local function AddLine(text, maxwidth, heightUsed, numQuestWatches, r, g, b)
 	current = current + 1
 	local l = lines[current]
 	l:SetText(text)
 	l:SetTextColor(r or .75, g or .61, b or 0)
-	return math.max(maxwidth, l:GetStringWidth()), heightUsed + l:GetHeight()
+	return math.max(maxwidth, l:GetStringWidth()), heightUsed + l:GetHeight(), numQuestWatches + 1
 end
 
 
 WatchFrame_AddObjectiveHandler(function(lineFrame, initialOffset, maxHeight, frameWidth)
-	local maxWidth = 0
-	local heightUsed = 0
+	local maxWidth, heightUsed, numQuestWatches = 0, 0, 0
 
 	f:SetPoint("TOPLEFT", lineFrame, "TOPLEFT", 0, initialOffset - WATCHFRAME_QUEST_OFFSET)
 
@@ -33,10 +32,10 @@ WatchFrame_AddObjectiveHandler(function(lineFrame, initialOffset, maxHeight, fra
 
 	for sender,values in pairs(Quecho.quests) do
 		if next(values)then
-			maxWidth, heightUsed = AddLine(sender, maxWidth, heightUsed)
-			for i,v in pairs(values) do maxWidth, heightUsed = AddLine(" - "..i..": "..v, maxWidth, heightUsed, 0.8, 0.8, 0.8) end
+			maxWidth, heightUsed, numQuestWatches = AddLine(sender, maxWidth, heightUsed, numQuestWatches)
+			for i,v in pairs(values) do maxWidth, heightUsed = AddLine(" - "..i..": "..v, maxWidth, heightUsed, numQuestWatches, 0.8, 0.8, 0.8) end
 		end
 	end
 
-	return heightUsed, maxWidth
+	return heightUsed, maxWidth, numQuestWatches
 end)
