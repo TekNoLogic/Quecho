f4ddc6b08f938f004bd4cfddd3da58068547f539
tekkub ʕ ´ᴥ` ʔ <git@tekkub.net>
2016-10-31 20:39:32 -0600

    Break frames out of tracker file

---
+++ frames/objective.lua
@@ -0,0 +1,52 @@
+
+local myname, ns = ...
+
+
+local lines = {}
+local parent = CreateFrame("Frame", nil, UIParent)
+parent:SetSize(1, 1)
+
+
+local function CreateLine()
+	local line = parent:CreateFontString(nil, nil, "GameFontNormal")
+	lines[line] = true
+	return line
+end
+
+
+local function GetUnusedLine()
+	for line in pairs[lines] do
+		if not line:IsShown() then
+			line:Show()
+			return line
+		end
+	end
+end
+
+
+function ns.GetLine()
+	return GetUnusedLine() or CreateLine()
+end
+
+
+function ns.HideLines()
+	for line in pairs(lines) do line:Hide() end
+end
+
+
+local function GetAnchor()
+	local block = BONUS_OBJECTIVE_TRACKER_MODULE.lastBlock
+	if block and block:IsShown() then return block end
+
+	block = AUTO_QUEST_POPUP_TRACKER_MODULE.lastBlock
+	if block and block:IsShown() then return block end
+
+	return QUEST_TRACKER_MODULE.lastBlock
+end
+
+
+function ns.AnchorLines(anchor)
+	local anchor = ObjectiveTrackerFrame.BlocksFrame
+	parent:SetPoint("TOPLEFT", anchor, 0, yoffset - 10)
+	parent:SetPoint("RIGHT", anchor, "RIGHT", 0, -10)
+end
---
+++ services/party_tracker.lua
@@ -0,0 +1,66 @@
+
+local myname, ns = ...
+
+
+local WATCHFRAME_INITIAL_OFFSET = WATCHFRAME_INITIAL_OFFSET or 0
+local WATCHFRAME_TYPE_OFFSET = WATCHFRAME_TYPE_OFFSET or 10
+
+
+local quests = ns.NewMemoizingTable(function() return {} end)
+
+
+local function AddLine(text, anchor, header)
+	local line = ns.GetLine()
+	line:SetText(text)
+
+	if header then
+		line:SetTextColor(0.7, 0.7, 0.9)
+	else
+		line:SetTextColor(0.75, 0.61, 0)
+	end
+
+	if anchor then
+		if header then
+			line:SetPoint("TOPLEFT", anchor, "BOTTOMLEFT", 0, -WATCHFRAME_TYPE_OFFSET)
+		else
+			line:SetPoint("TOPLEFT", anchor, "BOTTOMLEFT")
+		end
+	else
+		line:SetPoint("TOPLEFT", 0, -WATCHFRAME_INITIAL_OFFSET)
+	end
+
+	return line
+end
+
+
+local function Update()
+	ns.HideLines()
+
+	local anchor
+	for sender,values in pairs(quests) do
+		if next(values) then
+			anchor = AddLine(sender, anchor, true)
+			for i,v in pairs(values) do
+				anchor = AddLine("-  "..i..": "..v, anchor, false)
+			end
+		end
+	end
+
+	ns.AnchorLines()
+end
+
+
+local function OnPartyExpire(self, message, sender, objective)
+	quests[sender][objective] = nil
+	Update()
+end
+
+
+function OnPartyProgress(self, message, sender, objective, progress)
+	quests[sender][objective] = progress
+	Update()
+end
+
+
+ns.RegisterCallback("_PARTY_EXPIRE", OnPartyExpire)
+ns.RegisterCallback("_PARTY_PROGRESS", OnPartyProgress)
